42ec9d9bf73eb39f3e0acb9cce1e84aef7f3cb47
Christian Kaltepoth <christian@kaltepoth.de>
2016-01-22 08:40:47 +0100

    More examples

---
+++ code/src/arrow-functions/es5-function.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
+var odd = numbers.filter(function(n) {
+  return n % 2 !== 0;
+});
+
+console.info(odd);
--- code/src/template-strings/template-strings.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ../../node-babel.sh
-
-const name = "Christian";
-const message = `Hello ${name}`;
-
-console.log(message);
